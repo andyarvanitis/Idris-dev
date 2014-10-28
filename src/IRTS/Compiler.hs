79b87d630f1800c0b1b6615ae932ea9a8cc6929e
Andy Arvanitis <andyarvanitis@gmail.com>
2014-10-27 20:38:58 -0700

    Add arg and return types to FFunction constructor

--- src/IRTS/Compiler.hs
+++ src/IRTS/Compiler.hs
@@ -464,9 +464,9 @@ doForeign vs env (_ : fgn : args)
         , io == txt "IO" 
         = FFunctionIO
 
-    mkIty' (App (App (P _ (UN ff) _) _) _) 
+    mkIty' (App (App (P _ (UN ff) _) aty) rty)
         | ff == txt "FFunction"
-        = FFunction
+        = FFunction (mkIty' aty) (mkIty' rty)
 
     mkIty' _ = FAny
 
@@ -487,7 +487,7 @@ doForeign vs env (_ : fgn : args)
     mkIty "FPtr"        = FPtr
     mkIty "FManagedPtr" = FManagedPtr
     mkIty "FUnit"       = FUnit
-    mkIty "FFunction"   = FFunction
+    mkIty "FFunction"   = FFunction FUnit FUnit
     mkIty "FFunctionIO" = FFunctionIO
     mkIty "FBits8x16"   = FArith (ATInt (ITVec IT8 16))
     mkIty "FBits16x8"   = FArith (ATInt (ITVec IT16 8))
